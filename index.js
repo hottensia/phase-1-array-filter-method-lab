18e4452e22e7bd9b9d9d0e158372481be41e5a90
Hottensiah Wanjiku <w.hottensiah96@gmail.com>
2024-04-06 21:08:38 +0300

    filter methodlab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, Bobby) {
+    return drivers.filter(driver => driver.toLowerCase() === Bobby.toLowerCase());
+}
+
+function fuzzyMatch(drivers, Bo) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(Bo.toLowerCase()))
+
+
+}
+function matchName(drivers,Bobby) {
+    return drivers.filter(driver => driver.name === Bobby);
+
+}
